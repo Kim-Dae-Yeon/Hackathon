5c902d1e555ddd851ed21dfd188969e41ff06aaa
Kim-Dae-Yeon <polaris6921@postech.ac.kr>
2015-11-14 19:50:05 +0900

    php 업데이트

---
+++ consumer_get.php
@@ -0,0 +1,25 @@
+﻿<meta charset = "UTF-8">
+
+<?php
+	$host = "localhost";
+	$username = "iri";
+	$password = "hackaton";
+	$db_name = "iri";
+
+	$con = mysql_connect("$host", "$username", "$password") or die ("cannot connect");
+	mysql_query("set names utf8");
+	mysql_select_db("$db_name") or die ("cannot select DB");
+	$sql = "select * from consumer";
+	$result = mysql_query($sql);
+	$json = array();
+
+	if(mysql_num_rows($result)) {
+		while($row=mysql_fetch_assoc($result)) {
+		$json['consumer'][]=$row;
+		}
+	}
+	mysql_close($con);
+///	echo "안녕하세요";
+	echo json_encode($json);
+?>
+
---
+++ food_get.php
@@ -0,0 +1,20 @@
+﻿<?php
+	$host = "localhost";
+	$username = "iri";
+	$password = "hackaton";
+	$db_name = "iri";
+
+	$con = mysql_connect("$host", "$username", "$password") or die ("cannot connect");
+	mysql_select_db("$db_name") or die ("cannot select DB");
+	$sql = "select * from food";
+	$result = mysql_query($sql);
+	$json = array();
+
+	if(mysql_num_rows($result)) {
+		while($row=mysql_fetch_assoc($result)) {
+		$json['food'][]=$row;
+		}
+	}
+	mysql_close($con);
+	echo json_encode($json);
+?>
